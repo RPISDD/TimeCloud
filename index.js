ef4999716905b7a6dfe01712936592debbf02f9d
malcolm <lorbem@rpi.edu>
2015-11-02 23:43:47 -0500

    experimental, supports friends module and full stack usage

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var moduleFactory = require('./lib/shared/ModuleFactory.js');
+var request = require('./lib/shared/Request.js');
+var reply = require('./lib/shared/Reply.js');
+var errorHandler = require('./lib/shared/ErrorHandler.js');
+var user = require('./lib/shared/User.js');
 exports.handler = function(event, context){
-
+    var callingFunction = context.functionName;
+    var token = event.sessionToken;
+    
+    var req = request(token,event,context);
+    var rep = reply(req);
+    var err = errorHandler(req);
+    //NEED TO LOOKUP IN USERDB FIRST, FIX
+    var usr = user(123456,token);
+    
+    var module = moduleFactory(callingFunction, usr, req, rep);
+    module.run();
 };
