b68cac99e9d5e51a2d5b346c22f46731d66689e7
Chris <admin@whoorah.com>
2015-11-05 03:06:35 -0500

    yolo

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var request = require('./lib/shared/Request.js');
 var reply = require('./lib/shared/Reply.js');
 var errorHandler = require('./lib/shared/ErrorHandler.js');
 var user = require('./lib/shared/User.js');
+var fs = require('fs');
+// sign with default (HMAC SHA256)
+var jwt = require('jsonwebtoken');
+
+// sign with RSA SHA256
+var cert = fs.readFileSync('rsa');  // get private key
+
 exports.handler = function(event, context){
     var callingFunction = context.functionName;
     var token = event.sessionToken;
@@ -11,8 +18,18 @@ exports.handler = function(event, context){
     var rep = reply(req);
     var err = errorHandler(req);
     //NEED TO LOOKUP IN USERDB FIRST, FIX
-    var usr = user(123456,token);
+    try {
+  		var decoded = jwt.verify(token, cert);
+	} 
+	catch(err) {
+		//err
+		console.log("token invalid");
+
+	}
+    var usr = user(decoded.RIN,token);
+    var usr = user()
     
     var module = moduleFactory(callingFunction, usr, req, rep);
     module.run();
 };
+
